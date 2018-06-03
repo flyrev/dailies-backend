0eea8354ef4e8eea77508f9d6cb664308dbef85e
Christian Neverdal <flyrev@gmail.com>
2018-06-03 22:40:23 +0200

    Initial backend with Java 8

---
+++ src/main/java/flyrev/dailiesbackend/DailiesBackendApplication.java
@@ -0,0 +1,12 @@
+package flyrev.dailiesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailiesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailiesBackendApplication.class, args);
+	}
+}
---
+++ src/test/java/flyrev/dailiesbackend/DailiesBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package flyrev.dailiesbackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DailiesBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
